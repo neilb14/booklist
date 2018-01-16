0a49eaced11e57e246450ee2d555fccec6f821d5
Neil Bourgeois <neil.bourgeois@gmail.com>
2018-01-16 08:06:45 -0700

    first POS trainer

--- sample.py
+++ sample.py
@@ -1,4 +1,7 @@
 import sys
 from nltk import word_tokenize, pos_tag, ne_chunk
 text = sys.argv[1]
+print('== POS Tagged: ')
+print(pos_tag(word_tokenize(text)))
+print('== ... and Chunked: ')
 print(ne_chunk(pos_tag(word_tokenize(text))))
